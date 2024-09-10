08dbb79438cc417ffe53dbbbd3816b8710c90e64
Laura ramirez lrodar23m@icloud.com <lrodar23m@icloud.com>
2024-09-10 16:54:37 -0500

    se creo restaurants_controller.rb y solo el archivo de las vistas

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,47 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created."
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:ruc, :commercial_name, :company_name, :phone, :mobile_phone, :address, :district, :province, :country, :reference, :user_id)
+  end
+end
