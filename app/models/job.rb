b8f2e59c612f0eecb6b8b423e75cf5723d97f689
Levi Apolinar <leviam51@gmail.com>
2024-09-12 16:37:35 -0500

    The job listing section has become dynamic

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,8 @@ class Job < ApplicationRecord
   belongs_to :restaurant
   has_many :job_users, dependent: :destroy
   has_many :favorites, dependent: :destroy
+
+  def location
+    "#{restaurant.province}, #{restaurant.country}"
+  end
 end
