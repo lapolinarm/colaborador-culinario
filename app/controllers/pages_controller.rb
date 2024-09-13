6cb0f25d5e3fee71ac4fc1b2d011ebe7d1b74642
Levi Apolinar <leviam51@gmail.com>
2024-09-13 00:36:20 -0500

    All the data displayed in the view is dynamic, including the counter, and needs to be adjusted to make it work better for new jobs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,13 @@ class PagesController < ApplicationController
 
   def home
     @jobs = Job.includes(:restaurant).all
+    @selected_job = Job.find(params[:job_id]) if params[:job_id].present?
+
+    if @selected_job
+      @time_remaining = calculate_time_remaining(@selected_job)
+    else
+      @time_remaining = 0
+    end
   end
 
   def about
@@ -10,4 +17,26 @@ class PagesController < ApplicationController
 
   def dashboard
   end
+
+  def calculate_time_remaining(job)
+    now = Time.current
+    job_date = job.date.to_time
+
+    # Combina la fecha con la hora de inicio y fin
+    start_time = job_date.change(hour: job.hour_start.hour, min: job.hour_start.min)
+    end_time = job_date.change(hour: job.hour_end.hour, min: job.hour_end.min)
+
+    # Si la hora de fin es antes de la hora de inicio, se asume que es el día siguiente
+    end_time += 1.day if end_time < start_time
+
+    # Calcula el tiempo restante
+    if now < start_time
+      return (start_time - now).to_i
+    elsif now > end_time
+      return 0
+    else
+      return (end_time - now).to_i
+    end
+  end
+
 end
