d4b0c590811117046771313d3bcf3e6dfb6872b9
Levi Apolinar <leviam51@gmail.com>
2024-09-13 23:57:38 -0500

    An attempt was made to fix the apply now button, validations must be done to verify if the data is being stored correctly

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,6 @@ class Job < ApplicationRecord
   has_many :job_users, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
-  # enum job_mode: { presencial: 0, remoto: 1, hibrido: 2 }
   enum job_mode: { presencial: 0, hibrido: 1 }
   enum function: { mozo: 0, azafata: 1, delivery: 2, lavaplatos: 3 }
 
