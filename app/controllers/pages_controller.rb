758d1003be2391ae670ecefadb2044b28ae481a3
luis roldan <roldanluis022@gmail.com>
2024-09-14 15:37:59 -0500

    validate that home apply now sends correct data and creates application, also validated routes for application and successfully got info from the url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,6 +37,20 @@ class PagesController < ApplicationController
   end
 
   def application
+    if current_user.owner?
+      @restaurants = current_user.restaurants
+      if params[:date].present? || params[:restaurant_id].present?
+        @restaurants = @restaurants.joins(:jobs).where(jobs: { date: params[:date] }) if params[:date].present?
+        @restaurants = @restaurants.where(id: params[:restaurant_id]) if params[:restaurant_id].present?
+      end
+      render 'owners_dashboard'
+    elsif current_user.collaborator?
+      @applied_jobs = current_user.jobs
+      @applied_jobs = @applied_jobs.where(job_users: { status: params[:status] }) if params[:status].present?
+      render 'pages/application'
+    else
+      redirect_to root_path
+    end
   end
 
   private
