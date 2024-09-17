64a3d6b3141bc8cdde46d385efa74a38bca2da43
Levi Apolinar <leviam51@gmail.com>
2024-09-16 23:25:56 -0500

    added style to home

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,6 @@ module ColaboradorCulinario
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # config.i18n.default_locale = :es
+    config.i18n.default_locale = :es
   end
 end
