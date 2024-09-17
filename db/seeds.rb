1f8e8a81a3fd9045e6c87ac8a2f87e9b201d76b0
Laura ramirez lrodar23m@icloud.com <lrodar23m@icloud.com>
2024-09-17 14:22:31 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,14 @@
 require 'active_support/all'
+require 'faker'
 
 puts "Borrando job_users..."
-JobUser.destroy_all # Elimina todas las job_users existentes
-
+JobUser.destroy_all
 puts "Borrando jobs..."
-Job.destroy_all # Elimina todas las jobs existentes
-
+Job.destroy_all
 puts "Borrando restaurants..."
-Restaurant.destroy_all # Elimina todas los restaurants existentes
-
+Restaurant.destroy_all
 puts "Borrando usuarios..."
-User.destroy_all # Elimina todos los usuarios existentes
+User.destroy_all
 
 puts "Reseteando PK ID..."
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
@@ -20,121 +18,109 @@ ActiveRecord::Base.connection.reset_pk_sequence!('job_users')
 ActiveRecord::Base.connection.reset_pk_sequence!('favorites')
 
 puts "=" * 50
-puts "Inicio el Seeds en las tablas"
+puts "Iniciando el seed en las tablas..."
 
-50.times do
-  instance = Util.new
+# Usuarios específicos
+users = [
+  { email: "jaime@mail.com", first_name: "Jaime", last_name: "Obispo", role: "collaborator" },
+  { email: "laura@mail.com", first_name: "Laura", last_name: "Ramírez", role: "collaborator" },
+  { email: "levi@mail.com", first_name: "Levi", last_name: "Apolinar", role: "owner" },
+  { email: "luis@mail.com", first_name: "Luis", last_name: "Roldán", role: "owner" }
+]
 
-  # Definimos los métodos de pago
-  typePayMethod = {
-    "Efectivo" => 0,
-    "Transferencia bancaria" => 1,
-    "Monederos digitales" => 2
-  }
-
-  valPayMethod = instance.valor_random_del_hash(typePayMethod)
-
-  # Creación de usuario
-  user = User.create!(
-    email: Faker::Internet.email,
-    dni: rand(10000000..99999999),
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
+users.each do |user_attrs|
+  User.create!(
+    email: user_attrs[:email],
+    first_name: user_attrs[:first_name],
+    last_name: user_attrs[:last_name],
+    password: '123456',
+    role: user_attrs[:role],
     mobile_phone: "99#{rand(1000000..9999999)}",
-    role: Util::COLABORADOR,
+    dni: rand(10000000..99999999),
     address: Faker::Address.full_address,
-    payment_method: valPayMethod,
-    bank: valPayMethod.eql?(1) ? ["BBVA", "BCP", "Interbank", "Banbif", "Scotiabank", "Other"].sample : "-",
+    payment_method: rand(0..2),  # Método de pago aleatorio
+    bank: ["BBVA", "BCP", "Interbank"].sample,
     name_holder: Faker::Name.first_name,
-    bank_account: valPayMethod.eql?(1) ? rand(1000000000..9999999999) : "-",
-    cci: valPayMethod.eql?(1) ? rand(10000000000000000000..99999999999999999999) : "-",
-    digital_wallet: valPayMethod.eql?(2) ? ["Yape", "Plin"].sample : "-",
-    password: '123456'
-  )
-
-  valRestaurant = instance.returnRestaurants
-  location = valRestaurant.to_s[-7..-1].strip[1..-2]
-
-  # Asignamos un código de país (alpha-2)
-  country_code = 'PE'  # Código de Perú
-
-  # Creación de restaurante
-  restaurant = Restaurant.create!(
-    ruc: "201#{rand(10000000..99999999)}",
-    commercial_name: valRestaurant[0...-7].strip,
-    company_name: "#{valRestaurant[0...-7].strip.to_s.upcase} S.A.C",
-    phone: "01#{rand(1000000..9999999)}",
-    mobile_phone: "9#{rand(10000000..99999999).to_s}",
-    address: instance.returnAddres(location),
-    district: instance.randomDistritos(location),
-    province: instance.returnProvince(location),
-    country: country_code,  # Código del país
-    reference: "XX_reference",
-    user: user
-  )
-
-  # Hashes de tipos de trabajo, modos y funciones (enums)
-  typeJob = {
-    "Fulltime" => 0,
-    "Parttime" => 1
-  }
-  valTypeJob = instance.valor_random_del_hash(typeJob)
-
-  typeJobMode = {
-    "Presencial" => 0,
-    "Hibrido" => 1
-  }
-  valTypeJobMode = instance.valor_random_del_hash(typeJobMode)
-
-  typeFunction = {
-    "Mozo" => 0,
-    "Azafata" => 1,
-    "Delivery" => 2,
-    "Lavaplatos" => 3
-  }
-  valTypeFunction = instance.valor_random_del_hash(typeFunction)
-
-  # Creación de trabajos
-  hourTemp = instance.returnMoment("time") # Hora de inicio
-  random_hours_sum = rand(2..4) # Hora de finalización aleatoria
-  randomPaymentHour = [20, 30, 40, 50].sample
-
-  job = Job.create!(
-    date: instance.returnMoment("date"),
-    hour_start: hourTemp,
-    hour_end: hourTemp + (random_hours_sum * 3600), # Añadimos horas aleatorias
-    payment_hour: randomPaymentHour,
-    job_type: valTypeJob,
-    job_mode: valTypeJobMode,
-    function: valTypeFunction,
-    description: instance.returnDescriptionJob(valTypeFunction),
-    responsibility: instance.returnresponsibilityJob(valTypeFunction),
-    requirement: instance.returnRequirementJob(valTypeFunction),
-    restaurant: restaurant
-  )
-
-  # Creación de JobUser
-  typeStatus = {
-    "aceptado" => 0,
-    "pendiente" => 1,
-    "rechazado" => 2
-  }
-  valtypeStatus = instance.valor_random_del_hash(typeStatus)
-
-  job_user = JobUser.create!(
-    status: valtypeStatus,
-    payment_method_used: valPayMethod,
-    amount_paid: randomPaymentHour,
-    user: user,
-    job: job
-  )
-
-  # Creación de favoritos
-  favorite = Favorite.create!(
-    user: user,
-    job: job
+    bank_account: rand(1000000000..9999999999),
+    cci: rand(10000000000000000000..99999999999999999999),
+    digital_wallet: ["Yape", "Plin"].sample
   )
 end
 
-puts "Terminando el Seeds"
+# Usuarios adicionales (colaboradores y owners)
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    password: '123456',
+    role: ["owner", "collaborator"].sample,
+    mobile_phone: "99#{rand(1000000..9999999)}",
+    dni: rand(10000000..99999999),
+    address: Faker::Address.full_address,
+    payment_method: rand(0..2),
+    bank: ["BBVA", "BCP", "Interbank"].sample,
+    name_holder: Faker::Name.first_name,
+    bank_account: rand(1000000000..9999999999),
+    cci: rand(10000000000000000000..99999999999999999999),
+    digital_wallet: ["Yape", "Plin"].sample
+  )
+end
+
+# Crear restaurantes para los owners
+owners = User.where(role: 'owner')
+
+owners.each do |owner|
+  5.times do
+    Restaurant.create!(
+      ruc: "201#{rand(10000000..99999999)}",
+      commercial_name: Faker::Restaurant.name,
+      company_name: "#{Faker::Company.name} S.A.C.",
+      phone: "01#{rand(1000000..9999999)}",
+      mobile_phone: "9#{rand(10000000..99999999)}",
+      address: Faker::Address.full_address,
+      district: Faker::Address.community,
+      province: Faker::Address.state,
+      country: 'PE',  # Código de Perú
+      reference: Faker::Address.secondary_address,
+      user: owner
+    )
+  end
+end
+
+# Crear trabajos para los colaboradores
+collaborators = User.where(role: 'collaborator')
+
+collaborators.each do |collaborator|
+  Restaurant.all.sample(3).each do |restaurant|
+    10.times do
+      # Crear trabajos con fechas, algunos expirados
+      date = rand(1..10) > 2 ? Faker::Date.between(from: Date.today, to: 2.months.from_now) : Faker::Date.between(from: 2.months.ago, to: Date.today)
+      job = Job.create!(
+        date: date,
+        hour_start: Time.now.beginning_of_day + rand(8..11).hours,
+        hour_end: Time.now.beginning_of_day + rand(12..16).hours,
+        payment_hour: [20, 30, 40, 50].sample,
+        job_type: rand(0..1),
+        job_mode: rand(0..1),
+        function: rand(0..3),
+        description: Faker::Job.field,
+        responsibility: Faker::Job.key_skill,
+        requirement: Faker::Job.education_level,
+        restaurant: restaurant
+      )
+
+      # Crear relación entre el colaborador y el trabajo (JobUser)
+      JobUser.create!(
+        status: rand(0..2),
+        payment_method_used: rand(0..2),
+        amount_paid: job.payment_hour,
+        user: collaborator,
+        job: job
+      )
+    end
+  end
+end
+
+puts "Seeds finalizados exitosamente."
 puts "=" * 50
