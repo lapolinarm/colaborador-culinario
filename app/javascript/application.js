6b2b265c470fb815a1db11e34af4fccc436468c2
Levi Apolinar <70377994+lapolinarm@users.noreply.github.com>
2024-09-18 15:54:38 -0500

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -11,18 +11,3 @@ import "bootstrap"
 // Importar Chartkick y Chart.bundle (para gráficos)
 import "chartkick"
 import "Chart.bundle"
-
-// Si usas tooltips o popovers de Bootstrap, necesitas inicializarlos manualmente
-document.addEventListener("DOMContentLoaded", () => {
-  // Inicializar tooltips de Bootstrap
-  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-
-  // Inicializar popovers de Bootstrap
-  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-  popoverTriggerList.map(function (popoverTriggerEl) {
-    return new bootstrap.Popover(popoverTriggerEl)
-  })
-})
